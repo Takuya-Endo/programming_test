2875a34b00737410a012ea7dd6cbdfcc37aaad22
Takuya-Endo <above5purple@gmail.com>
2021-06-27 10:55:42 +0000

    [Add]ピタゴラス数

---
+++ pythagorean_theorem.rb
@@ -0,0 +1,45 @@
+def pythagorean_triple_array
+
+  max = 100
+  
+  pythagorean_triple = []
+  
+  for a in 1..max do
+    for b in 1..max do
+      for c in 1..max do
+        if a**2 + b**2 == c**2
+          abc = [a, b, c]
+          pythagorean_triple.push(abc)
+        end
+      end
+    end
+  end
+  
+  puts pythagorean_triple
+
+end
+
+
+def pythagorean_triple_abc
+  
+  max = 500
+  
+  for a in 1..max do
+    for b in a..max do
+      for c in 1..a+b do
+        if a**2 + b**2 == c**2
+          if a + b + c == 1000
+            abc = [a, b, c]
+            break
+          end
+        end
+      end
+    end
+  end
+  
+  puts abc
+  
+end
+
+puts pythagorean_triple_abc
+
